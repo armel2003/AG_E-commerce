eec6e212b273591ed54f704250e3984b2900d9c9
Nathanaelle-creator <nathanaelle.latouche@epitech.eu>
2025-07-30 15:47:37 +0200

    ADD: ajout du ficier boutique

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -6,6 +6,7 @@ import ProductDetail from './components/ProductDetail.jsx';
 import HomePage from './page/HomePage.jsx';
 import RegisterForm from './page/RegisterForm.jsx';
 import Moncompte from './page/Moncompte.jsx';
+import Boutique from './page/Boutique.jsx';
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/login" element={<LoginForm/>}/>
                 <Route path="/register" element={<RegisterForm/>}/>
                 <Route path="/account/:id" element={<Moncompte/>}/>
+                <Route path="/boutique" element={<Boutique/>}/>
             </Routes>
         </>
     );
