7cdc6c76162fa8fea3072b2c62ccc4f9a7bdf730
Nathanaelle-creator <nathanaelle.latouche@epitech.eu>
2025-07-31 15:36:45 +0200

    UPDATE:modification du fichier app pour revoir les routes

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-// import LoginForm from "./page/LoginForm.jsx";
-// import ProductDetail from './components/ProductDetail.jsx';
-import HomePage from './page/HomePage.jsx';
-// import RegisterForm from './page/RegisterForm.jsx';
-// import Moncompte from './page/Moncompte.jsx';
-// import HomePage from './components/HomePage.jsx';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
+//user
+import HomePage from './page/HomePage.jsx';
+import LoginForm from './page/LoginForm.jsx';
+import RegisterForm from './page/RegisterForm.jsx';
+import Moncompte from './page/Moncompte.jsx';
+//import Boutique from './page/Boutique.jsx';
+import ProductDetail from './components/ProductDetail.jsx';
+
+// admin
 import AdminLayout from './pageAdmin/AdminLayout.jsx';
 import AdminDashboard from './pageAdmin/AdminDashboard.jsx';
 import CreateArticle from './pageAdmin/CreateArticle.jsx';
@@ -16,33 +18,33 @@ import EditArticle from './pageAdmin/EditArticle.jsx';
 import UserManagement from './pageAdmin/UserManagement.jsx';
 import EditCategory from './pageAdmin/EditCategorie.jsx';
 import CreatCategory from './pageAdmin/CreatCategory.jsx';
-//import ProductDetail from './DetailProduct/ProductDetail';
-
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminLayout />}>
-          {/* Routes enfants de AdminLayout */}
-          <Route index element={<AdminDashboard />} />
-          <Route path="dashboard" element={<AdminDashboard />} />
-          <Route path="create-article" element={<CreateArticle />} />
-          <Route path="create-category" element={<CreatCategory />} />
-          <Route path=":id/edit" element={<EditArticle />} />
-          <Route path=":id/category/edit" element={<EditCategory />} />
-          <Route path="user-management" element={<UserManagement />} />
-        </Route>
-        {/* <Route path="/login" element={<LoginForm/>}/>
-        <Route path="/register" element={<RegisterForm/>}/>
-        <Route path="/account/:id" element={<Moncompte/>}/> */}
-        {/* Rediriger par défaut vers /admin */}
-        <Route path="/" element={<Navigate to="/admin" replace />} />
-        <Route path="*" element={<Navigate to="/admin" replace />} />
-        {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
-      </Routes>
-    </>
+    <Routes>
+      {/*  utilisateur */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/account/:id" element={<Moncompte />} />
+      {/* <Route path="/boutique" element={<Boutique />} /> */}
+      <Route path="/product/:id" element={<ProductDetail />} />
+
+      {/* s admin */}
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
+        <Route path="create-article" element={<CreateArticle />} />
+        <Route path="create-category" element={<CreatCategory />} />
+        <Route path=":id/edit" element={<EditArticle />} />
+        <Route path=":id/category/edit" element={<EditCategory />} />
+        <Route path="user-management" element={<UserManagement />} />
+      </Route>
+
+      {/* Redirections */}
+      <Route path="/admin/*" element={<Navigate to="/admin" replace />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
