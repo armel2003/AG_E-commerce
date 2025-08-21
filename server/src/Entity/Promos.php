557869138e61b5bea51e432d4ba960f037a50f66
Nathanaelle-creator <nathanaelle.latouche@epitech.eu>
2025-08-21 10:54:14 +0200

    ADD: ajout de la db pour réaliser les promos

---
+++ server/src/Entity/Promos.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PromosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PromosRepository::class)]
+class Promos
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'promos')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?product $product_id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductId(): ?product
+    {
+        return $this->product_id;
+    }
+
+    public function setProductId(?product $product_id): static
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(?float $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
